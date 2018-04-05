bc4330ee7ea1297c5a72209e9e8ae5daf599c683
raco82 <mail.racorp@gmail.com>
2018-04-05 18:43:30 +0200

    on y est presque

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
-let monTitre = document.querySelector('googlelogo');
-monTitre.textContent = 'Hello world!';
+function toggle(content)
+{
+	node = document.getElementById(content);
+	if (node.style.visibility=="hidden")
+	{
+		// Contenu caché, le montrer
+		node.style.visibility = "visible";
+		node.style.height = "auto";			// Optionnel rétablir la hauteur
+	}
+	else
+	{
+		// Contenu visible, le cacher
+		node.style.visibility = "hidden";
+		node.style.height = "0";			// Optionnel libérer l'espace
+	}
+}
